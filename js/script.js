f5172aca5e3946d97d0f9a1ec6dc59acaea3d7d8
pbanana2 <peelbanana773@gmail.com>
2026-06-02 11:36:19 +0200

    2nd commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("keydown", (event) =>{
+    document.getElementById(event.code).style="color:blue";
+});
+
+document.addEventListener("keyup", (event) =>{
+    document.getElementById(event.code).style="event.code.color";
+});
