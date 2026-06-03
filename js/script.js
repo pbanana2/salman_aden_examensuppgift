c5b582f87ccd8e516c0e8c17e58178ab24274b15
pbanana2 <peelbanana773@gmail.com>
2026-06-03 11:22:25 +0200

    ommit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
+let textDiv = document.getElementById("textDiv")
+
 document.addEventListener("keydown", (event) =>{
     let code = document.getElementById(event.code);
     console.log(event)
     if(code != CapsLock){
         code.style="background-color:blue"
+        if (AltRight.style.backgroundColor != "blue"){
+            if (code === Backquote || code === Digit1 || code === Digit2 || code === Digit3 
+                    || code === Digit5 || code === Digit6 || code === Digit7 
+                    || code === Digit0 || code == Minus ||code === KeyQ
+                    || code === KeyW || code === KeyE || code === KeyR || code === KeyT || code === KeyY 
+                    || code === KeyU || code === KeyI || code === KeyO || code === KeyP 
+                    || code === KeyA || code === KeyS || code === KeyD || code === KeyF 
+                    || code === KeyG || code === KeyH || code === KeyJ || code === KeyK 
+                    || code === KeyK || code === KeyL || code === KeyZ || code === KeyX 
+                    || code === KeyC || code === KeyB || code === KeyN || code === KeyM
+                    || code === Comma || code === Period || code === Slash || code === BracketLeft
+                    || code === Semicolon || code === Quote || code === IntlBackslash){
+                let text = document.createTextNode(event.key);
+                textDiv.append(text)
+            }
+            else if(code === Digit4 || code === Digit8 || code === Digit9){
+                if (ShiftLeft.style.backgroundColor != "blue" && ShiftRight.style.backgroundColor != "blue"){
+                    let text = document.createTextNode(event.key);
+                    textDiv.append(text)
+                }
+            }
+        }
+        else if(code === Delete){
+            textDiv.remove(Text)
+        }
     }
     else{
         if(code.style.backgroundColor != "blue"){
