77f52c96caec53d7771f45fb0fda58e102c4813d
pbanana2 <peelbanana773@gmail.com>
2026-06-08 18:58:49 +0200

    commit game

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 let main = document.getElementById("main")
+let message = document.getElementById("message")
 let textDiv = document.getElementById("textDiv")
 let visual = document.getElementById("visual")
-
-const letters_5 = ["lugia", "klink", "snivy", "zubat", "eevee"]
-const letters_6 = ["flygon", "mewtwo", "komala", "lombre", "gengar"]
-const letters_7 = ["scyther", "rhyhorn", "pikachu", "spearow", "machoke"]
-const letters_8 = ["dragalge", "dusknoir", "inteleon", "charizard", "blastoise"]
-const letters_9 = ["pachirisu", "gardevoir", "toucannon", "scorbunny", "bulbasaur"]
+let answer = ""
+let pokemon_image = document.getElementById("pokemon_image")
 
 document.addEventListener("keydown", (event) =>{
     let code = document.getElementById(event.code);
-
+    console.log(event)
     if(code != CapsLock){
         document.getElementsByClassName("R").style="animation-duration: 1s"
         code.style="background-color:blue"
@@ -29,12 +26,18 @@ document.addEventListener("keydown", (event) =>{
                 let text = document.createTextNode(event.key);
                 textDiv.appendChild(text)
                 textDiv.appendChild(visual)
+                answer += event.key
+                var audio = new Audio('filer/click.mp3')
+                audio.play()
             }
             else if(code === Digit4 || code === Digit8 || code === Digit9){
                 if (ShiftLeft.style.backgroundColor != "blue" && ShiftRight.style.backgroundColor != "blue"){
                     let text = document.createTextNode(event.key);
                     textDiv.appendChild(text)
                     textDiv.appendChild(visual)
+                    answer += event.key
+                    var audio = new Audio('filer/click.mp3')
+                    audio.play()
                 }
             }
             else if(code === KeyP){
@@ -43,17 +46,49 @@ document.addEventListener("keydown", (event) =>{
                 textDiv.appendChild(visual)
                 var audio = new Audio('filer/fart.mp3')
                 audio.play()
+                answer += event.key
             }
         }
 
         if(code === Backspace){
             textDiv.removeChild(visual.previousSibling)
+            answer = answer.slice(0, -1)
+            var audio = new Audio('filer/slip.mp3')
+            audio.play()
         }
+
         if(code === Delete){
             textDiv.innerText = ""
             textDiv.appendChild(visual)
+            answer = ""
+            var audio = new Audio('filer/delete.mp3')
+            audio.play()
         }
 
+        if(code === Enter){
+            event.preventDefault()
+            if(playing == "yes"){
+                if (answer.toLowerCase() == pokemon){
+                    var audio = new Audio('')
+                    index = current_list.indexOf(pokemon)
+                    current_list.splice(index, 1)
+                    alert("correct!")
+                    pokemon_image.src=""
+                    answer = ""
+                    textDiv.innerText = ""
+                    textDiv.appendChild(visual)
+                    playing = "no"
+                }
+                else{
+                    var audio = new Audio('filer/dead.mp3')
+                    audio.play()
+                    alert("incorrect!")
+                }
+            }
+        }
+        if(code === Space){
+            event.preventDefault()
+        }
     }
     else{
         if(code.style.backgroundColor != "blue"){
@@ -73,3 +108,52 @@ document.addEventListener("keyup", (event) =>{
         code.style.backgroundColor = "black"
     }
 })
+
+const letters_5 = ["lugia", "klink", "snivy", "zubat", "eevee", "absol", "rotom", "bagon"]
+const letters_6 = ["flygon", "mewtwo", "komala", "lombre", "gengar", "piplup", "bidoof"]
+const letters_7 = ["scyther", "rhyhorn", "pikachu", "spearow", "machoke", "dratini", "altaria"]
+const letters_8 = ["giratina", "dusknoir", "inteleon", "charizard", "blastoise", "regirock", "vaporeon"]
+const letters_9 = ["pachirisu", "gardevoir", "toucannon", "scorbunny", "bulbasaur", "tyranitar"]
+var pokemon
+var current_list
+var playing = "no"
+
+function random_5(){
+    playing = "yes"
+    message.innerHTML = "Who's that pokemon?"
+    current_list = letters_5
+    pokemon = letters_5[Math.floor(Math.random() * letters_5.length)]
+    document.getElementById("pokemon_image").src="filer/pokemon/" + pokemon + ".avif"
+}
+
+function random_6(){
+    playing = "yes"
+    message.innerHTML = "Who's that pokemon?"
+    current_list = letters_6
+    pokemon = letters_6[Math.floor(Math.random() * letters_6.length)]
+    document.getElementById("pokemon_image").src="filer/pokemon/" + pokemon + ".avif"
+}
+
+function random_7(){
+    playing = "yes"
+    message.innerHTML = "Who's that pokemon?"
+    current_list = letters_7
+    pokemon = letters_7[Math.floor(Math.random() * letters_7.length)]
+    document.getElementById("pokemon_image").src="filer/pokemon/" + pokemon + ".avif"
+}
+
+function random_8(){
+    playing = "yes"
+    message.innerHTML = "Who's that pokemon?"
+    current_list = letters_8
+    pokemon = letters_8[Math.floor(Math.random() * letters_8.length)]
+    document.getElementById("pokemon_image").src="filer/pokemon/" + pokemon + ".avif"
+}
+
+function random_9(){
+    playing = "yes"
+    message.innerHTML = "Who's that pokemon?"
+    current_list = letters_9
+    pokemon = letters_9[Math.floor(Math.random() * letters_9.length)]
+    document.getElementById("pokemon_image").src="filer/pokemon/" + pokemon + ".avif"
+}
